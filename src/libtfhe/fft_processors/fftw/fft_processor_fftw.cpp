4e595b9fba7b7dea37c6547561eeb604f42ec71d
Jon <john_akke@hotmail.com>
2020-03-25 09:57:39 +0100

    Embedding python interpreter in fft_processor_fftw.cpp, build file changes to accomodate this.

--- src/libtfhe/fft_processors/fftw/fft_processor_fftw.cpp
+++ src/libtfhe/fft_processors/fftw/fft_processor_fftw.cpp
@@ -7,6 +7,11 @@
 #include <cmath>
 #include <mutex>
 
+#include <pybind11/embed.h>
+
+static void fftw_execute_wrapper(fftw_plan p);
+static int hasRun = 0;
+
 FFT_Processor_fftw::FFT_Processor_fftw(const int32_t N): _2N(2*N),N(N),Ns2(N/2) {
     rev_in = (double*) malloc(sizeof(double) * _2N);
     out = (double*) malloc(sizeof(double) * _2N);
@@ -32,7 +37,7 @@ void FFT_Processor_fftw::execute_reverse_int(cplx* res, const int* a) {
     cplx* rev_out_cplx = (cplx*) rev_out; //fftw_complex and cplx are layout-compatible
     for (int32_t i=0; i<N; i++) rev_in[i]=a[i]/2.;
     for (int32_t i=0; i<N; i++) rev_in[N+i]=-rev_in[i];
-    fftw_execute(rev_p);
+    fftw_execute_wrapper(rev_p);
     for (int32_t i=0; i<Ns2; i++) res[i]=rev_out_cplx[2*i+1];
     for (int32_t i=0; i<=Ns2; i++) assert(abs(rev_out_cplx[2*i])<1e-20);
 }
@@ -42,7 +47,7 @@ void FFT_Processor_fftw::execute_reverse_torus32(cplx* res, const Torus32* a) {
     cplx* rev_out_cplx = (cplx*) rev_out; //fftw_complex and cplx are layout-compatible
     for (int32_t i=0; i<N; i++) rev_in[i]=aa[i]*_2pm33;
     for (int32_t i=0; i<N; i++) rev_in[N+i]=-rev_in[i];
-    fftw_execute(rev_p);
+    fftw_execute_wrapper(rev_p);
     for (int32_t i=0; i<Ns2; i++) res[i]=rev_out_cplx[2*i+1];
     for (int32_t i=0; i<=Ns2; i++) assert(abs(rev_out_cplx[2*i])<1e-20);
 }
@@ -52,23 +57,37 @@ void FFT_Processor_fftw::execute_direct_Torus32(Torus32* res, const cplx* a) {
     cplx* in_cplx = (cplx*) in; //fftw_complex and cplx are layout-compatible
     for (int32_t i=0; i<=Ns2; i++) in_cplx[2*i]=0;
     for (int32_t i=0; i<Ns2; i++) in_cplx[2*i+1]=a[i];
-    fftw_execute(p);
+    fftw_execute_wrapper(p);
     for (int32_t i=0; i<N; i++) res[i]=Torus32(int64_t(out[i]*_1sN*_2p32));
     //pas besoin du fmod... Torus32(int64_t(fmod(rev_out[i]*_1sN,1.)*_2p32));
     for (int32_t i=0; i<N; i++) assert(fabs(out[N+i]+out[i])<1e-20);
 }
 
+static void fftw_execute_wrapper(fftw_plan p) {
+    char *overlay;
+    overlay = getenv("PYTHON_OVERLAY");
+
+    if (overlay != NULL && !hasRun) {
+        pybind11::scoped_interpreter guard{};
+
+        pybind11::module o = pybind11::module::import(overlay);
+        pybind11::object result = o.attr("execute")(2,2);
+        hasRun++;
+    }
+    fftw_execute(p);
+}
+
 FFT_Processor_fftw::~FFT_Processor_fftw() {
     fftw_destroy_plan(p);
     fftw_destroy_plan(rev_p);
-    fftw_free(in); fftw_free(rev_out);	
+    fftw_free(in); fftw_free(rev_out);
     free(rev_in); free(out);
     delete[] omegaxminus1;
 }
 
 
 /**
- * FFT functions 
+ * FFT functions
  */
 EXPORT void IntPolynomial_ifft(LagrangeHalfCPolynomial* result, const IntPolynomial* p) {
     fp1024_fftw.execute_reverse_int(((LagrangeHalfCPolynomial_IMPL*)result)->coefsC, p->coefs);
